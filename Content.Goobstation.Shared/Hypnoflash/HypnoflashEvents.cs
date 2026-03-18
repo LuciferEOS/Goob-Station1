1a6c59839605d37806b04fd434beb6a8e29ae130
Shegare <228shadowfox228@gmail.com>
2026-03-19 00:24:54 +0300

    fix

--- Content.Goobstation.Shared/Hypnoflash/HypnoflashEvents.cs
+++ Content.Goobstation.Shared/Hypnoflash/HypnoflashEvents.cs
@@ -1,17 +1,13 @@
-using Robust.Shared.Serialization;
-
 namespace Content.Goobstation.Shared.Hypnoflash;
 
 /// <summary>
 /// raised on the entities in the hypnoflash radius
 /// </summary>
-[Serializable, NetSerializable]
 [ByRefEvent]
 public readonly partial record struct HypnoflashedEvent;
 
 /// <summary>
 /// raised on the entity that originally activated the hypnoflash
 /// </summary>
-[Serializable, NetSerializable]
 [ByRefEvent]
 public readonly partial record struct HypnoflashActivatedEvent;
--- Content.Goobstation.Shared/Hypnoflash/HypnotizedConditionComponent.cs
+++ Content.Goobstation.Shared/Hypnoflash/HypnotizedConditionComponent.cs
@@ -1,4 +1,4 @@
 namespace Content.Goobstation.Shared.Hypnoflash;
 
 [RegisterComponent]
-public sealed partial class HypnotizedConditionComponent : Component {}
+public sealed partial class HypnotizedConditionComponent : Component;
